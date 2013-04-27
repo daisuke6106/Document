4f9c9c9805bdd9f139265ef5b8e97dede397d1a2
dk <dk@425babaa-d5dd-46a5-afc5-8c713db7da9f>
2013-04-27 06:08:01 +0000

    Documentプロジェクトのテストコード整備
    git-svn-id: file:///opt/svn/repository/projects/trunk@74 425babaa-d5dd-46a5-afc5-8c713db7da9f

--- test/jp/co/dk/document/html/TestHtmlElementFactory.java
+++ test/jp/co/dk/document/html/TestHtmlElementFactory.java
@@ -1,5 +1,9 @@
 package jp.co.dk.document.html;
 
+import static org.junit.Assert.*;
+
+import java.util.List;
+
 import mockit.Expectations;
 import mockit.Mocked;
 import mockit.NonStrictExpectations;
@@ -12,38 +16,50 @@ import jp.co.dk.document.foundation.TestDocumentFoundation;
 import jp.co.dk.document.html.constant.HtmlElementName;
 import jp.co.dk.document.message.DocumentMessage;
 
-public class TestHtmlElementFactory extends TestDocumentFoundation {
+public class TestHtmlElementFactory  {
+	
+//	@Mocked
+//	private jp.co.dk.document.html.HtmlElement htmlElement;
+//	
+//	@Test
+//	public void convert() {
+//		jp.co.dk.document.html.HtmlDocument htmlDocument = createHtmlDocument();
+//		jp.co.dk.document.xml.XmlDocument   xmlDocument  = createXmlDocument();
+//		HtmlElementFactory factory = new HtmlElementFactory();
+//		
+//		// HTML要素以外を渡した場合、例外が発生すること。
+//		try {
+//			factory.convert(xmlDocument.getElement().get(0));
+//		} catch (DocumentException e) {
+//			if (e.getMessageObj() != DocumentMessage.ERROR_ELEMENT_CONVERT) {
+//				fail(e);
+//			}
+//		}
+//		
+//		// HTML要素から要素タイプを検索した際にnullが返却された場合、HTML要素がそのまま返却されること。
+//		
+//		// モックオブジェクトを取得
+//		new Expectations() {{htmlElement.getElementType();result = HtmlElementName.A;}};
+//		try {
+////			Element htmlElement   = htmlDocument.getElement().get(0);
+//			Element returnElement = factory.convert(this.htmlElement);
+//			assertEquals(htmlElement, returnElement);
+//		} catch (DocumentException e) {
+//			if (e.getMessageObj() != DocumentMessage.ERROR_ELEMENT_CONVERT) {
+//				fail(e);
+//			}
+//		}
+//	}
 	
 	@Mocked
-	private jp.co.dk.document.html.HtmlElement htmlElement;
-	
+	List<String> stub; // スタブオブジェクトを宣言
+
 	@Test
-	public void convert() {
-		jp.co.dk.document.html.HtmlDocument htmlDocument = createHtmlDocument();
-		jp.co.dk.document.xml.XmlDocument   xmlDocument  = createXmlDocument();
-		HtmlElementFactory factory = new HtmlElementFactory();
-		
-		// HTML要素以外を渡した場合、例外が発生すること。
-		try {
-			factory.convert(xmlDocument.getElement().get(0));
-		} catch (DocumentException e) {
-			if (e.getMessageObj() != DocumentMessage.ERROR_ELEMENT_CONVERT) {
-				fail(e);
-			}
-		}
-		
-		// HTML要素から要素タイプを検索した際にnullが返却された場合、HTML要素がそのまま返却されること。
-		
-		// モックオブジェクトを取得
-		new Expectations() {{htmlElement.getElementType();result = null;}};
-		try {
-			Element htmlElement   = htmlDocument.getElement().get(0);
-			Element returnElement = factory.convert(htmlElement);
-			assertEquals(htmlElement, returnElement);
-		} catch (DocumentException e) {
-			if (e.getMessageObj() != DocumentMessage.ERROR_ELEMENT_CONVERT) {
-				fail(e);
-			}
-		}
+	public void スタブメソッドの定義() {
+	    new NonStrictExpectations() {{
+	        stub.get(0); result = "Hello"; // スタブメソッドの定義
+	    }};
+	    System.out.println(stub.get(0));
+//	    assertTrue(stub.get(0).equals("Hello")); // 検証
 	}
 }
