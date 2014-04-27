c126f967c5772f561e59c7c1b26e2b11e7954b60
dk <dk@425babaa-d5dd-46a5-afc5-8c713db7da9f>
2014-04-27 15:05:45 +0000

    Documentのテストコードを改修
    git-svn-id: file:///opt/svn/repository/projects/trunk@454 425babaa-d5dd-46a5-afc5-8c713db7da9f

--- test/jp/co/dk/document/html/element/SelectTest.java
+++ test/jp/co/dk/document/html/element/SelectTest.java
@@ -54,4 +54,127 @@ public class SelectTest extends DocumentFoundationTest {
 			assertNull(selectedOption);
 		} catch (HtmlDocumentException e) {	fail(e);}
 	}
+	
+	@Test
+	public void getDefaultSlectedOption() {
+		
+		try {
+			StringBuilder sb1 = new StringBuilder();
+			sb1.append("<select name=\"example\">");
+			sb1.append("<option value=\"サンプル1\">サンプル1</option>");
+			sb1.append("<option value=\"サンプル2\">サンプル2</option>");
+			sb1.append("<option value=\"サンプル3\">サンプル3</option>");
+			sb1.append("</select>");
+			Select select = new Select(new HtmlElement(sb1.toString(), new HtmlElementFactory()));
+			Option selectedOption = select.getDefaltSelectedOption();
+			assertNull(selectedOption);
+		} catch (HtmlDocumentException e) {	fail(e);}
+		
+		try {
+			StringBuilder sb2 = new StringBuilder();
+			sb2.append("<select name=\"example\">");
+			sb2.append("<option value=\"サンプル1\" selected>サンプル1</option>");
+			sb2.append("<option value=\"サンプル2\">サンプル2</option>");
+			sb2.append("<option value=\"サンプル3\">サンプル3</option>");
+			sb2.append("</select>");
+			Select select = new Select(new HtmlElement(sb2.toString(), new HtmlElementFactory()));
+			Option selectedOption = select.getDefaltSelectedOption();
+			assertThat(selectedOption.getAttribute("value"), is ("サンプル1"));
+		} catch (HtmlDocumentException e) {	fail(e);}
+		
+		try {
+			StringBuilder sb3 = new StringBuilder();
+			sb3.append("<select name=\"example\">");
+			sb3.append("<option value=\"サンプル1\">サンプル1</option>");
+			sb3.append("<option value=\"サンプル2\" selected>サンプル2</option>");
+			sb3.append("<option value=\"サンプル3\">サンプル3</option>");
+			sb3.append("</select>");
+			Select select = new Select(new HtmlElement(sb3.toString(), new HtmlElementFactory()));
+			Option selectedOption = select.getDefaltSelectedOption();
+			assertThat(selectedOption.getAttribute("value"), is ("サンプル2"));
+		} catch (HtmlDocumentException e) {	fail(e);}
+	}
+	
+	@Test
+	public void select() {
+		// OPTION用を保持していないselectに対してインデックスを指定した場合、falseが返却されること。
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("<select name=\"example\">");
+			sb.append("</select>");
+			Select select = new Select(new HtmlElement(sb.toString(), new HtmlElementFactory()));
+			assertThat(select.select(0), is(false));
+			assertNull(select.getSelectedOption());
+			assertNull(select.getDefaltSelectedOption());
+		} catch (HtmlDocumentException e) {	fail(e);}
+		
+		// 3のみOPTION要素を保持するSELECTに対して4つ目を選択するようにした場合、falseが返却されること。
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("<select name=\"example\">");
+			sb.append("<option value=\"サンプル1\">サンプル1</option>");
+			sb.append("<option value=\"サンプル2\" selected>サンプル2</option>");
+			sb.append("<option value=\"サンプル3\">サンプル3</option>");
+			sb.append("</select>");
+			Select select = new Select(new HtmlElement(sb.toString(), new HtmlElementFactory()));
+			assertThat(select.select(4), is(false));
+			assertThat(select.getSelectedOption().getValue(), is ("サンプル2"));
+			assertThat(select.getDefaltSelectedOption().getValue(), is ("サンプル2"));
+		} catch (HtmlDocumentException e) {	fail(e);}
+		
+		// 存在するインデックスを指定した場合、trueが返却されること。
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("<select name=\"example\">");
+			sb.append("<option value=\"サンプル1\">サンプル1</option>");
+			sb.append("<option value=\"サンプル2\" selected>サンプル2</option>");
+			sb.append("<option value=\"サンプル3\">サンプル3</option>");
+			sb.append("</select>");
+			Select select = new Select(new HtmlElement(sb.toString(), new HtmlElementFactory()));
+			assertThat(select.select(4), is(false));
+			assertThat(select.getSelectedOption().getValue(), is ("サンプル2"));
+			assertThat(select.getDefaltSelectedOption().getValue(), is ("サンプル2"));
+		} catch (HtmlDocumentException e) {	fail(e);}
+	}
+	
+	@Test
+	public void isDisabled() {
+		// disabledタグが設定されていない場合、falseが返却されること
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("<select name=\"example\">");
+			sb.append("<option value=\"サンプル1\">サンプル1</option>");
+			sb.append("<option value=\"サンプル2\">サンプル2</option>");
+			sb.append("<option value=\"サンプル3\">サンプル3</option>");
+			sb.append("</select>");
+			Select select = new Select(new HtmlElement(sb.toString(), new HtmlElementFactory()));
+			assertThat(select.isDisabled(), is(false));
+		} catch (HtmlDocumentException e) {	fail(e);}
+		
+		// disabledタグが設定されていない場合、trueが返却されること
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("<select name=\"example\" disabled>");
+			sb.append("<option value=\"サンプル1\">サンプル1</option>");
+			sb.append("<option value=\"サンプル2\">サンプル2</option>");
+			sb.append("<option value=\"サンプル3\">サンプル3</option>");
+			sb.append("</select>");
+			Select select = new Select(new HtmlElement(sb.toString(), new HtmlElementFactory()));
+			assertThat(select.isDisabled(), is(true));
+		} catch (HtmlDocumentException e) {	fail(e);}
+	}
+	
+	@Test
+	public void getMessage() {
+		try {
+			StringBuilder sb = new StringBuilder();
+			sb.append("<select name=\"example\">");
+			sb.append("<option value=\"サンプル1\">サンプル1</option>");
+			sb.append("<option value=\"サンプル2\" selected>サンプル2</option>");
+			sb.append("<option value=\"サンプル3\">サンプル3</option>");
+			sb.append("</select>");
+			Select select = new Select(new HtmlElement(sb.toString(), new HtmlElementFactory()));
+			assertThat(select.getMessage(), is("example="));
+		} catch (HtmlDocumentException e) {fail(e);}
+	}
 }
