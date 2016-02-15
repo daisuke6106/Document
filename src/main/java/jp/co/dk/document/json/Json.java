d4951f709ecaddc4ddc5c046cb221fc2598f6d15
Daisuke Kanno <daisuke6106@gmail.com>
2016-02-16 03:04:54 +0900

    JSONドキュメント用クラスを追加

---
+++ src/main/java/jp/co/dk/document/json/Json.java
@@ -0,0 +1,56 @@
+package jp.co.dk.document.json;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.arnx.jsonic.JSON;
+import net.arnx.jsonic.JSONException;
+import jp.co.dk.document.File;
+import jp.co.dk.document.exception.DocumentException;
+import static jp.co.dk.document.message.DocumentMessage.*;
+
+/**
+ * Jsonは、ネットワーク上に存在するJSONを表すオブジェクトです
+ * 
+ * @author D.Kanno
+ */
+public class Json extends File {
+	
+	/** JSONデータオブジェクト */
+	protected Object data;
+	
+	/**
+	 * <p>コンストラクタ</p>
+	 * 
+	 * @param inputStream 読み込みストリーム
+	 * @throws DocumentException 読み込みストリームからの読み込みに失敗した場合
+	 */
+	public Json(InputStream inputStream) throws DocumentException {
+		super(inputStream);
+		try {
+			this.data = JSON.decode(this.fileData.getStream());
+		} catch (JSONException | IOException e) {
+			throw new DocumentException(ERROR_DECORD_TO_JSON, e);
+		}
+	}
+	
+	/**
+	 * JSONデータをリストとして返却します。
+	 * @return JSONデータのリスト
+	 */
+	public List getDataByList() {
+		return new ArrayList((List)this.data);
+	}
+	
+	/**
+	 * JSONデータをリストとして返却します。
+	 * @return JSONデータのリスト
+	 */
+	public Map getDataByMap() {
+		return new LinkedHashMap((Map)this.data);
+	}
+}
---
+++ src/main/java/jp/co/dk/document/json/package-info.java
@@ -0,0 +1,6 @@
+/**
+  * JSONのドキュメント操作を行うクラスを保持するパッケージ。
+  * @since 1.0
+  * @author D.Kanno
+  */
+package jp.co.dk.document.json;
